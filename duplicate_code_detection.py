fa0c40373ab8c5db50569bcfb16f0285414ea463
Viktor Vestlund <vikves@student.chalmers.se>
2021-12-01 14:47:02 +0100

    Updated duplicated code is printed in logfile
    Updated the existing code with the addition of finding the longest common substring and then printing it into a log file for inspection. This will lead to better understandability of where the duplication actually occurs. The threshold of how long a common substring can be changed as an argument to the command, furthermore the amount of printed common substrings can also be altered via an argument. The default is the three longest common substrings. For the threshold, it starts at 25 to not include duplicates that are common and not a code smell, such as different keywords.

--- duplicate_code_detection.py
+++ duplicate_code_detection.py
@@ -10,15 +10,16 @@ import argparse
 import gensim
 import tempfile
 import json
+import logging
+from textwrap import indent
 from enum import Enum
 from nltk.tokenize import word_tokenize
 from collections import OrderedDict
 
-source_code_file_extensions = ["h", "c", "cpp", "cc", "java", "py", "cs"]
+source_code_file_extensions = ["h", "c", "cpp", "cc", "java", "py", "cs", "gd"]
 file_column_label = "File"
 similarity_column_label = "Similarity (%)"
 similarity_label_length = len(similarity_column_label)
-
 
 class ReturnCode(Enum):
     SUCCESS = 0
@@ -72,6 +73,10 @@ def main():
     parser.add_argument("--ignore-files", nargs="+", help="Files to ignore.")
     parser.add_argument("-j", "--json", type=bool,
                         default=False, help="Print output as JSON.")
+    parser.add_argument("-i", "--iterations", type=int, default = 3,
+                        help="Number of iterations of duplicated code to print to the logfile.")
+    parser.add_argument("-th", "--threshold", type=int, default = 25,
+                        help="Lowest threshold of duplication length when detected.")
     parser.add_argument("--project-root-dir", type=str, default=str(),
                         help="The relative path to the project root directory to be removed when printing out results.")
     parser.add_argument("--file-extensions", nargs="+", default=source_code_file_extensions,
@@ -81,14 +86,14 @@ def main():
     args = parser.parse_args()
 
     result = run(args.fail_threshold, args.directories, args.files, args.ignore_directories,
-                 args.ignore_files, args.json, args.project_root_dir, args.file_extensions,
+                 args.ignore_files, args.json, args.iterations, args.threshold, args.project_root_dir, args.file_extensions,
                  args.ignore_threshold)
 
     return result
 
 
 def run(fail_threshold, directories, files, ignore_directories, ignore_files,
-        json_output, project_root_dir, file_extensions, ignore_threshold):
+        json_output, iterations, threshold, project_root_dir, file_extensions, ignore_threshold):
     # Determine which files to compare for similarities
     source_code_files = list()
     files_to_ignore = list()
@@ -131,7 +136,7 @@ def run(fail_threshold, directories, files, ignore_directories, ignore_files,
 
     # Find the largest string length to format the textual output
     largest_string_length = len(max(source_code_files, key=len).replace(project_root_dir, ""))
-
+    
     # Parse the contents of all the source files
     source_code = OrderedDict()
     for source_code_file in source_code_files:
@@ -154,13 +159,17 @@ def run(fail_threshold, directories, files, ignore_directories, ignore_files,
 
     exit_code = ReturnCode.SUCCESS
     code_similarity = dict()
+
+    # Logging codeduplication
+    logging.basicConfig(filename="codeDuplication.txt", format='%(message)s', filemode= "w")
+
     for source_file in source_code:
         # Check for similarities
         query_doc = [w.lower()
                      for w in word_tokenize(source_code[source_file])]
         query_doc_bow = dictionary.doc2bow(query_doc)
+        mostFreqWord = query_doc_bow
         query_doc_tf_idf = tf_idf[query_doc_bow]
-
         short_source_file_path = source_file.replace(project_root_dir, '')
         conditional_print("\n\n\n" + CliColors.HEADER +
                           "Code duplication probability for " + short_source_file_path + CliColors.ENDC, json_output)
@@ -187,8 +196,19 @@ def run(fail_threshold, directories, files, ignore_directories, ignore_files,
                 exit_code = ReturnCode.THRESHOLD_EXCEEDED
             color = CliColors.OKGREEN if similarity_percentage < 10 else (
                 CliColors.WARNING if similarity_percentage < 20 else CliColors.FAIL)
-            conditional_print("%s     " % (short_source_path.ljust(largest_string_length)) +
+            conditional_print("%s       " % (short_source_path.ljust(largest_string_length)) +
                               color + "%.2f" % (similarity_percentage) + CliColors.ENDC, json_output)
+            if similarity_percentage > 20:
+                # In i lcs(source_code[baseFilen], source_code[jämför], basefilen, jämför)
+                # Base filen
+                # shourt_source_file_path / source_file
+                # Filen vi jämför med
+                #short_source_path / source
+                lcs(source_code[short_source_file_path], source_code[short_source_path], short_source_file_path, short_source_path, iterations, threshold)
+            
+            
+        
+    
     if exit_code == ReturnCode.THRESHOLD_EXCEEDED:
         conditional_print(
             "Code duplication threshold exceeded. Please consult logs.", json_output)
@@ -196,8 +216,81 @@ def run(fail_threshold, directories, files, ignore_directories, ignore_files,
         similarities_json = json.dumps(code_similarity, indent=4)
         print(similarities_json)
 
+    conditional_print("\n\nDetailed information about the duplicated lines are outputted into codeDuplication.txt", json_output)
     return (exit_code, code_similarity)
 
+def lcs(S,T, fileName1, fileName2, iterations, threshold):
+    #creates tempS and tempT, these will be reduced by LCS for each iteration
+    tempS=S
+    tempT=T
+    
+    
+    finalList=list()
+
+    #Range is number of iterations to run through
+    for i in range(0,iterations):
+
+
+        Sstart=0
+        Tstart=0
+        m = len(tempS)
+        n = len(tempT)
+        counter = [[0]*(n+1) for x in range(m+1)]
+        longest = 0
+        lcs_set = set()
+        for i in range(m):
+            for j in range(n):
+                if tempS[i] == tempT[j]:
+                    c = counter[i][j] + 1
+                    counter[i+1][j+1] = c
+                    if c > longest:
+                        lcs_set = list()
+                        longest = c
+                        lcs_set.append(tempS[i-c+1:i+1])
+                        Sstart=i
+                        Tstart=j
+                    elif c == longest:
+                        lcs_set.append(tempS[i-c+1:i+1])
+
+
+        for i in lcs_set:
+            if len(i)>threshold:
+                startsubS=S[:S.find(i)]
+                startsubT=T[:T.find(i)]
+
+                endsubS=S[S.find(i):S.rfind(i)+len(i)]
+                endsubT=T[T.find(i):T.rfind(i)+len(i)]
+
+
+                SstartRow=startsubS.count('\n')+1
+                TstartRow=startsubT.count('\n')+1
+
+                SendRow=endsubS.count('\n')+SstartRow
+                TendRow=endsubT.count('\n')+TstartRow
+
+                Srows=(SstartRow, SendRow)
+                Trows=(TstartRow ,TendRow)
+
+                leng = max(len(f'{fileName1} rows: {SstartRow} - {SendRow}'), len(f'{fileName2} rows: {TstartRow} - {TendRow}'))
+
+                logging.warning('-' * (leng + 1))
+                logging.warning(f'{fileName1} rows: {SstartRow} - {SendRow}')
+                logging.warning(f'{fileName2} rows: {TstartRow} - {TendRow}')
+                logging.warning('-' * (leng + 1))
+                logging.warning('duplication found:')
+                logging.warning(" ")
+                logging.warning(indent(endsubS, '\t'))
+                logging.warning('-' * (leng + 1))
+                logging.warning("\n")
+
+
+        for i in lcs_set:
+            tempS=tempS.replace(i,'')
+            tempT=tempT.replace(i,'')
+
+
+    
+    return finalList
 
 if __name__ == "__main__":
     exit_code, _ = main()
